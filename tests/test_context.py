03d271f2eee1b2d989ab7ad0b6116e92c2c52396
Damien Baty <damien.baty@polyconseil.fr>
2021-01-09 16:31:53 +0100

    tests: Add minimal tests
    We're far from a reasonable coverage, obviously. But that's a good
    start! :)

--- tests/test_context.py
+++ tests/test_context.py
@@ -13,3 +13,14 @@ class TestExtractDomainFromRemoteUrl:
         domain, path = context._extract_domain_and_path(url)
         assert domain == "github.com"
         assert path == "Polyconseil/cogite"
+
+
+def test_get_context():
+    ctx = context.get_context()
+    assert ctx.host_domain == 'github.com'
+    assert ctx.repository == 'cogite'
+    # Unfortunately, it's hard to test more than that:
+    # - remote_url: could be HTTP or SSH, could be the origin
+    #   repository or a fork.
+    # - owner: could be Polyconseil, or the owner of the fork
+    # - branch: the current branch, which we don't control.
